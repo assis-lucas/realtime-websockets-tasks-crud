3c39e738bc69874e6f79a7fa4a6818fd5de7c16f
Lucas Assis <lucasassistomaz@gmail.com>
2021-10-20 07:34:06 -0300

    changing .env.example + cluster added to .env values

--- realtime-frontend/src/services/pusher.js
+++ realtime-frontend/src/services/pusher.js
@@ -4,9 +4,14 @@ if (!process.env.REACT_APP_PUSHER_APP_KEY) {
   throw new Error("Your pusher app key is not found on your .env!");
 }
 
+if (!process.env.REACT_APP_PUSHER_CLUSTER) {
+  throw new Error("Your pusher app cluster is not found on your .env!");
+}
+
+
 const pusher = new Pusher(process.env.REACT_APP_PUSHER_APP_KEY, {
   forceTLS: true,
-  cluster: "us2",
+  cluster: process.env.REACT_APP_PUSHER_CLUSTER,
 });
 
 export default pusher;
